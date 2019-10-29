2b1ea2b911fdb26a11921da8d276e36f3ee6c821
aparna19000 <aparna19000@gmail.com>
2019-10-29 17:24:18 +0530

    Add Slingshot

--- sketch.js
+++ sketch.js
@@ -17,12 +17,12 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    slingShot = new Ground(150,305,50,100);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+  
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -59,4 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    slingShot.display();
 }
